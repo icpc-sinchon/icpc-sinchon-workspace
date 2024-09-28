d56f3aff76fd60c5389ad1e241b13aa4d55ea912
witch-factory <soakdma37@gmail.com>
2024-09-28 21:02:47 +0900

    deps: prisma 라이브러리 버전 업데이트, prisma 타입 모노레포 전반에 공유

--- apps/admin-backend/types/index.ts
+++ apps/admin-backend/types/index.ts
@@ -1,86 +1,86 @@
 import type {
-	RefundPolicy,
-	Lecture,
-	Semester,
-	Student,
-	Prisma,
-} from "@prisma/client";
+  RefundPolicy,
+  Lecture,
+  Semester,
+  Student,
+  Prisma,
+} from "@icpc-sinchon/shared";
 
 export type EmptyObject = Record<string, never>;
 
 export type BojLoginCredentials = {
-	boj_id: string;
-	boj_pw: string;
+  boj_id: string;
+  boj_pw: string;
 };
 
 export type SemesterQuery = {
-	year: string;
-	season: Semester["season"];
+  year: string;
+  season: Semester["season"];
 };
 
 export type SemesterIdentifier = {
-	year: Semester["year"];
-	season: Semester["season"];
+  year: Semester["year"];
+  season: Semester["season"];
 };
 
 export type LectureQuery = {
-	year: string;
-	season: Semester["season"];
-	lectureLevel: Lecture["level"];
+  year: string;
+  season: Semester["season"];
+  lectureLevel: Lecture["level"];
 };
 
 export type LectureIdentifier = {
-	year: Semester["year"];
-	season: Semester["season"];
-	level: Lecture["level"];
+  year: Semester["year"];
+  season: Semester["season"];
+  level: Lecture["level"];
 };
 
 export type StudentPatchBody = {
-	attendLog: StudentAttendance["attendLog"];
+  attendLog: StudentAttendance["attendLog"];
 } & LectureQuery;
 
 export type NewStudent = {
-	name: string;
-	bojHandle: string;
-	email: string;
-	phone: string;
-	school: Student["school"];
-	studentNumber: string;
-	paymentStatus: Student["paymentStatus"];
-	refundAccount: string;
-	lectureInfo: LectureIdentifier;
+  name: string;
+  bojHandle: string;
+  email: string;
+  phone: string;
+  school: Student["school"];
+  studentNumber: string;
+  paymentStatus: Student["paymentStatus"];
+  refundAccount: string;
+  lectureInfo: LectureIdentifier;
 };
 
 // TODO: 모노레포 등으로 타입 통합하기
 export type StudentAttendance = {
-	studentId: number;
-	lectureId: number;
-	name: string;
-	bojHandle?: string;
-	refundAccount?: string;
-	refundAmount?: number;
-	paymentStatus: Student["paymentStatus"];
-	attendLog: {
-		round: number;
-		lectureDone: boolean;
-		taskDone: boolean;
-	}[];
+  studentId: number;
+  lectureId: number;
+  name: string;
+  bojHandle?: string;
+  refundAccount?: string;
+  refundAmount?: number;
+  paymentStatus: Student["paymentStatus"];
+  attendLog: {
+    round: number;
+    lectureDone: boolean;
+    taskDone: boolean;
+  }[];
 };
 
 export type RefundPolicyInput = Omit<
-	Prisma.RefundPolicyCreateManyInput,
-	"semesterId"
+  Prisma.RefundPolicyCreateManyInput,
+  "semesterId"
 > &
-	SemesterIdentifier;
+  SemesterIdentifier;
 
 export type RefundPolicyVerification = {
-	minAttend: number;
-	maxAttend: number;
-	type: RefundPolicy["type"];
+  minAttend: number;
+  maxAttend: number;
+  type: RefundPolicy["type"];
 };
 
 export type LectureInput = {
-	level: Lecture["level"];
-	lectureNumber: number;
-	bojGroupId: number;
+  level: Lecture["level"];
+  lectureNumber: number;
+  bojGroupId: number;
 } & SemesterQuery;
