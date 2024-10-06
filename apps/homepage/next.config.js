6f9d19859df1ae8f0e909593446af91540f6e1ef
witch-factory <soakdma37@gmail.com>
2024-10-06 22:50:10 +0900

    dep: styled components 버전업, next에 적용

--- apps/homepage/next.config.js
+++ apps/homepage/next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   /* config options here */
   pageExtensions: ["js", "jsx", "ts", "tsx"],
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 module.exports = nextConfig;
---
+++ apps/homepage/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@components/button";
+import styled from "styled-components";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default HomePage;
