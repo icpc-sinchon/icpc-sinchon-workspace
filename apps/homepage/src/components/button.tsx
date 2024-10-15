adaf64243ef534588563697ed8709a99e5d4c906
witch-factory <soakdma37@gmail.com>
2024-10-15 18:35:35 +0900

    minor: 사용하지 않을 컴포넌트들을 제거

--- apps/homepage/src/components/button.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import styled from "styled-components";
-
-export const Button = styled.button`
-  background-color: red;
-  color: white;
-  padding: 10px;
-  border-radius: 5px;
-`;
--- apps/homepage/src/components/ui/flex-box.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const FlexBox = styled((props) => {
-  return <div {...props} />;
-})`
-  display: flex;
-`;
