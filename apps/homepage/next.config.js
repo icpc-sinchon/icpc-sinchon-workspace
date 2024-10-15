e6bf340d75c7acf4382488e8018219934caefffc
witch-factory <soakdma37@gmail.com>
2024-10-15 13:45:13 +0900

    dep: css 라이브러리를 vanilla extract로 바꾸고 next 세팅

--- apps/homepage/next.config.js
+++ apps/homepage/next.config.js
@@ -1,17 +1,12 @@
 // @ts-check
 const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
-const withVanillaExtract = createVanillaExtractPlugin({
-  // identifiers: "short",
-});
+const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
   pageExtensions: ["js", "jsx", "ts", "tsx"],
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
 };
 
 module.exports = withVanillaExtract(nextConfig);
