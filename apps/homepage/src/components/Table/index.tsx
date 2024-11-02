d06439249e967810afeefc66deb05ce3c78d3ab7
witch-factory <soakdma37@gmail.com>
2024-11-02 21:47:48 +0900

    feat: 데이터를 보여주는 테이블 컴포넌트 제작

---
+++ apps/homepage/src/components/Table/index.tsx
@@ -0,0 +1,63 @@
+import * as styles from "./styles.css";
+
+// 링크도 표시할 수 있다.
+type Value = number | string | { label: string; url: string };
+
+type Column<T> = {
+  key: keyof T;
+  header: string;
+};
+
+type Item = {
+  [key: string]: Value;
+};
+
+type TableProps<T extends Item> = {
+  data: T[];
+  columns: Column<T>[];
+};
+
+function renderValue(value: Value) {
+  if (typeof value === "object") {
+    return (
+      <a
+        href={value.url}
+        target="_blank"
+        rel="noreferrer"
+        className={styles.link}
+      >
+        {value.label}
+      </a>
+    );
+  }
+  return value;
+}
+
+function DataTable<T extends Item>({ columns, data }: TableProps<T>) {
+  return (
+    <table className={styles.table}>
+      <thead>
+        <tr>
+          {columns.map((column) => (
+            <th key={String(column.key)} className={styles.tableHeader}>
+              {column.header}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, rowIndex) => (
+          <tr key={rowIndex} className={styles.tableRow}>
+            {columns.map((column) => (
+              <td key={String(column.key)} className={styles.tableData}>
+                {renderValue(row[column.key])}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default DataTable;
