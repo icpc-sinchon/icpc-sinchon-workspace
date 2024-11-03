7f436183543e40835e3a8e4da0fc35c2e00a7791
witch-factory <soakdma37@gmail.com>
2024-11-03 11:55:01 +0900

    fix: 명예의 전당 데이터 포매팅 시 멘토가 없어도 빈 배열을 리턴하던 문제 수정

--- apps/homepage/src/utils/formatHallOfFameData.ts
+++ apps/homepage/src/utils/formatHallOfFameData.ts
@@ -1,25 +1,35 @@
 import type { HallOfFame } from "src/types/hallOfFame";
 
-export const formatHallOfFameData = (data: HallOfFame) => {
+function formatContributor(data: HallOfFame["campContributor"][0]) {
   const formattedData = {
-    ...data,
-    campContributor: data.campContributor.map((camp) => ({
-      level: camp.level,
-      lecturer: camp.lecturer.map((lecturer) => ({
-        ...lecturer,
-        bojHandle: {
-          label: lecturer.bojHandle,
-          url: `https://www.acmicpc.net/user/${lecturer.bojHandle}`,
-        },
-      })),
-      mentor: (camp.mentor ?? []).map((mentor) => ({
+    level: data.level,
+    lecturer: data.lecturer.map((lecturer) => ({
+      ...lecturer,
+      bojHandle: {
+        label: lecturer.bojHandle,
+        url: `https://www.acmicpc.net/user/${lecturer.bojHandle}`,
+      },
+    })),
+    ...(data.mentor && {
+      mentor: data.mentor.map((mentor) => ({
         ...mentor,
         bojHandle: {
           label: mentor.bojHandle,
           url: `https://www.acmicpc.net/user/${mentor.bojHandle}`,
         },
       })),
-    })),
+    }),
+  };
+
+  return formattedData;
+}
+
+export const formatHallOfFameData = (data: HallOfFame) => {
+  const formattedData = {
+    ...data,
+    campContributor: data.campContributor.map((camp) =>
+      formatContributor(camp),
+    ),
   };
   return formattedData;
 };
