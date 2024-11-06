68706a94590acea3e3a4fd95bc26063a0e513074
Junghwa <jungh150@naver.com>
2024-11-07 03:17:15 +0900

    feat: semester.service.ts에 생성, 조회, 수정, 삭제 함수 추가

--- apps/admin-backend/src/semester/semester.service.ts
+++ apps/admin-backend/src/semester/semester.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import type { Prisma, Semester } from "@prisma/client";
+import type { SemesterRepository } from "./semester.repository";
 
 @Injectable()
-export class SemesterService {}
+export class SemesterService {
+  constructor(private semesterRepository: SemesterRepository) {}
+
+  async createSemester(params: { data: Prisma.SemesterCreateInput }) {
+    const { data } = params;
+    const semester = await this.semesterRepository.createSemester({ data });
+    return semester;
+  }
+
+  async getSemesters(): Promise<Semester[]> {
+    const semesters = await this.semesterRepository.getSemesters();
+    return semesters;
+  }
+
+  async getSemesterById(params: { id: number }): Promise<Semester | null> {
+    const { id } = params;
+    const semester = await this.semesterRepository.getSemester({
+      where: { id },
+    });
+    return semester;
+  }
+
+  async updateSemester(params: { where: Prisma.SemesterWhereUniqueInput; data: Prisma.SemesterUpdateInput }) {
+    const { where, data } = params;
+    const semester = await this.semesterRepository.updateSemester({
+      where,
+      data,
+    });
+    return semester;
+  }
+
+  async deleteSemester(params: { where: Prisma.SemesterWhereUniqueInput }) {
+    const { where } = params;
+    const semester = await this.semesterRepository.deleteSemester({ where });
+    return semester;
+  }
+}
