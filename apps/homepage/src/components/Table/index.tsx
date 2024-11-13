42ac47f4af4c8de4b3a58cbedcbce4c3ada292cc
witch-factory <soakdma37@gmail.com>
2024-11-13 22:01:48 +0900

    fix: 테이블 관련 타입 이름을 더 뜻을 드러내도록 변경

--- apps/homepage/src/components/Table/index.tsx
+++ apps/homepage/src/components/Table/index.tsx
@@ -3,22 +3,23 @@ import * as styles from "./styles.css";
 // 링크도 표시할 수 있다.
 type Value = number | string | React.ReactNode;
 
-type Column<T> = {
+export type Column<T> = {
   key: keyof T;
   header: string;
 };
 
-type Item = {
+export type TableItem = {
   [key: string]: Value;
 };
 
-type TableProps<T extends Item> = {
+type TableProps<T extends TableItem> = {
   data: T[];
   columns: Column<T>[];
   fixedLayout?: boolean;
 };
 
-function DataTable<T extends Item>({
+// TODO: 테이블의 각 컬럼 너비 조절 기능?
+function DataTable<T extends TableItem>({
   columns,
   data,
   fixedLayout,
