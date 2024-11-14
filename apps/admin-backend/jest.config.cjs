d60ce3952e33cdcb25df799474e14ab81e7c4a40
Junghwa <jungh150@naver.com>
2024-11-15 01:22:09 +0900

    feat: jest 환경 수정 및 테스트 코드 초안 작성

---
+++ apps/admin-backend/jest.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+.tsx?$": ["ts-jest", {}],
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.spec.ts",
+  ],
+  coverageReporters: ["text", "lcov"],
+};
