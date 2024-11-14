13786dfce6dec24a252103db89c119e5f5cc9664
witch-factory <soakdma37@gmail.com>
2024-11-14 12:52:07 +0900

    feat: 후원사 로고 표시 UI

---
+++ apps/homepage/src/ui/LogoSection/styles.css.ts
@@ -0,0 +1,27 @@
+import { style } from "@vanilla-extract/css";
+
+export const container = style({
+  display: "flex",
+  flexDirection: "column",
+  gap: "0.5rem",
+});
+
+export const imageContainer = style({
+  display: "flex",
+  flexDirection: "row",
+  overflow: "auto",
+  gap: "1.5rem",
+  justifyContent: "start",
+  flexWrap: "wrap",
+  marginTop: "1rem",
+});
+
+export const logoWrapper = style({
+  height: "2rem",
+});
+
+export const logo = style({
+  width: "auto",
+  height: "2rem",
+  objectFit: "contain",
+});
