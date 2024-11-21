5680d6b77b36d7223e438e4311527bd7a657d7e1
jungh150 <jungh150@naver.com>
2024-11-21 21:37:49 +0900

    feat: semester controller에 대한 테스트 코드 작성

--- apps/admin-backend/test/semester/semester.service.spec.ts
+++ apps/admin-backend/test/semester/semester.service.spec.ts
@@ -2,8 +2,8 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { SemesterService } from "../../src/semester/semester.service";
 import { SemesterRepository } from "../../src/semester/semester.repository";
 import { NotFoundException } from "@nestjs/common";
-import { UpdateSemesterDto } from "src/semester/dto/update-semester.dto";
 import { CreateSemesterDto } from "src/semester/dto/create-semester.dto";
+import { Season } from "@prisma/client";
 
 const mockSemesterRepository = {
   createSemester: jest.fn(),
@@ -34,22 +34,15 @@ describe("SemesterService", () => {
 
   describe("createSemester", () => {
     test("학기를 생성하고 반환해야 합니다", async () => {
-      const createSemesterDto = {
+      const createSemesterDto: CreateSemesterDto = {
         year: 2024,
-        season: "Spring",
-        lecture: [],
-        refundPolicy: [],
+        season: Season.Spring, // Season Enum 사용
       };
-      const createdSemester = {
-        id: 1,
-        ...createSemesterDto,
-      };
+      const createdSemester = { id: 1, ...createSemesterDto };
 
       mockSemesterRepository.createSemester.mockResolvedValue(createdSemester);
 
-      const result = await semesterService.createSemester(
-        createSemesterDto as CreateSemesterDto,
-      );
+      const result = await semesterService.createSemester(createSemesterDto);
       expect(result).toEqual(createdSemester);
       expect(mockSemesterRepository.createSemester).toHaveBeenCalledTimes(1);
       expect(mockSemesterRepository.createSemester).toHaveBeenCalledWith({
@@ -61,8 +54,8 @@ describe("SemesterService", () => {
   describe("getSemesters", () => {
     test("모든 학기를 반환해야 합니다", async () => {
       const semesters = [
-        { id: 1, year: 2024, season: "Spring", lecture: [], refundPolicy: [] },
-        { id: 2, year: 2023, season: "Fall", lecture: [], refundPolicy: [] },
+        { id: 1, year: 2024, season: Season.Spring },
+        { id: 2, year: 2023, season: Season.Fall },
       ];
 
       mockSemesterRepository.getSemesters.mockResolvedValue(semesters);
@@ -76,13 +69,7 @@ describe("SemesterService", () => {
 
   describe("getSemesterById", () => {
     test("ID로 특정 학기를 반환해야 합니다", async () => {
-      const semester = {
-        id: 1,
-        year: 2024,
-        season: "Spring",
-        lecture: [],
-        refundPolicy: [],
-      };
+      const semester = { id: 1, year: 2024, season: Season.Spring };
 
       mockSemesterRepository.getSemester.mockResolvedValue(semester);
 
@@ -109,24 +96,15 @@ describe("SemesterService", () => {
 
   describe("updateSemester", () => {
     test("학기를 수정하고 반환해야 합니다", async () => {
-      const updateSemesterDto = { lecture: [], refundPolicy: [] };
-      const existingSemester = {
-        id: 1,
-        year: 2024,
-        season: "Spring",
-        lecture: [],
-        refundPolicy: [],
-      };
-      const updatedSemester = { ...existingSemester, ...updateSemesterDto };
+      const updateSemesterDto = { year: 2025, season: Season.Fall };
+      const existingSemester = { id: 1, year: 2024, season: Season.Spring };
+      const updatedSemester = { id: 1, ...updateSemesterDto };
 
       // 기존 학기를 찾도록 mock 설정
       mockSemesterRepository.getSemester.mockResolvedValue(existingSemester);
       mockSemesterRepository.updateSemester.mockResolvedValue(updatedSemester);
 
-      const result = await semesterService.updateSemester(
-        1,
-        updateSemesterDto as UpdateSemesterDto,
-      );
+      const result = await semesterService.updateSemester(1, updateSemesterDto);
 
       expect(result).toEqual(updatedSemester);
       expect(mockSemesterRepository.getSemester).toHaveBeenCalledTimes(1);
@@ -141,15 +119,12 @@ describe("SemesterService", () => {
     });
 
     test("존재하지 않는 학기를 수정하려고 하면 NotFoundException을 던져야 합니다", async () => {
-      const updateSemesterDto = { lecture: [], refundPolicy: [] };
+      const updateSemesterDto = { year: 2025, season: Season.Fall };
 
       mockSemesterRepository.getSemester.mockResolvedValue(null);
 
       await expect(
-        semesterService.updateSemester(
-          999,
-          updateSemesterDto as UpdateSemesterDto,
-        ),
+        semesterService.updateSemester(999, updateSemesterDto),
       ).rejects.toThrow(NotFoundException);
 
       expect(mockSemesterRepository.getSemester).toHaveBeenCalledTimes(1);
@@ -161,13 +136,7 @@ describe("SemesterService", () => {
 
   describe("deleteSemester", () => {
     test("학기를 삭제하고 삭제된 학기를 반환해야 합니다", async () => {
-      const existingSemester = {
-        id: 1,
-        year: 2024,
-        season: "Spring",
-        lecture: [],
-        refundPolicy: [],
-      };
+      const existingSemester = { id: 1, year: 2024, season: Season.Spring };
 
       // 기존 학기를 찾도록 mock 설정
       mockSemesterRepository.getSemester.mockResolvedValue(existingSemester);
