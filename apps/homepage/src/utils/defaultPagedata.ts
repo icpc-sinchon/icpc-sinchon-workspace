022f462693e913b3e1c2fcde91190d0a1df9dd91
witch-factory <soakdma37@gmail.com>
2024-12-13 17:48:48 +0900

    fix: 데이터가 아직 존재하지 않는 학기의 경우 더미 파일을 만드는 대신 기본 문구를 출력하도록 수정

---
+++ apps/homepage/src/utils/defaultPagedata.ts
@@ -0,0 +1,62 @@
+import type { DataType, Semester } from "src/types";
+import type { CampContest } from "src/types/campContest";
+import type { CampHistory } from "src/types/campHistory";
+import type { HallOfFame } from "src/types/hallOfFame";
+import type { SUAPCData } from "src/types/suapc";
+
+const makeCampContestData = (semester: Semester): CampContest => {
+  return {
+    ...semester,
+    dateTime:
+      "아직 2025 Winter 시즌이 진행되지 않았습니다. 역대 캠프 정보는 상단의 탭을 참고해주세요.",
+    links: {},
+    contest: [],
+  };
+};
+
+const makeCampHistoryData = (semester: Semester): CampHistory => {
+  return {
+    ...semester,
+    study: [],
+  };
+};
+
+const makeHallOfFameData = (semester: Semester): HallOfFame => {
+  return {
+    ...semester,
+    duration:
+      "아직 2025 Winter 시즌이 진행되지 않았습니다. 역대 캠프 정보는 상단의 탭을 참고해주세요.",
+    manager: [],
+    campContributor: [],
+  };
+};
+
+const makeSUAPCData = (semester: Semester): SUAPCData => {
+  return {
+    ...semester,
+    dateTime:
+      "아직 2025 Winter 시즌이 진행되지 않았습니다. 역대 캠프 정보는 상단의 탭을 참고해주세요.",
+    links: {},
+    contest: [],
+    setter: [],
+    reviewer: [],
+    sponsor: [],
+  };
+};
+
+export function makeDefaultPageData<T extends DataType>(
+  semester: Semester,
+  dataType: T,
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+): any {
+  switch (dataType) {
+    case "campContest":
+      return makeCampContestData(semester);
+    case "campHistory":
+      return makeCampHistoryData(semester);
+    case "hallOfFame":
+      return makeHallOfFameData(semester);
+    case "suapc":
+      return makeSUAPCData(semester);
+  }
+}
