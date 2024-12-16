d770cc07594b1beb6609148f46d82767b5b33f6f
witch-factory <soakdma37@gmail.com>
2024-12-16 18:27:01 +0900

    fix: 유니언 타입 대신 prisma의 Season enum을 쓰도록 변경

--- apps/admin-backend/src/semester/entities/semester.entity.ts
+++ apps/admin-backend/src/semester/entities/semester.entity.ts
@@ -1,7 +1,7 @@
-import { Semester } from "@prisma/client";
+import { Season, Semester } from "@prisma/client";
 
 export class SemesterEntity implements Semester {
   id: number;
   year: number;
-  season: "Spring" | "Summer" | "Fall" | "Winter";
+  season: Season;
 }
