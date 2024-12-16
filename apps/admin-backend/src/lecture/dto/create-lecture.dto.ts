2f69d6c25e14bc42fce7ccc2d5464b5a79d2df11
witch-factory <soakdma37@gmail.com>
2024-12-16 20:56:39 +0900

    feat: lecture dto, 엔티티

--- apps/admin-backend/src/lecture/dto/create-lecture.dto.ts
+++ apps/admin-backend/src/lecture/dto/create-lecture.dto.ts
@@ -1,1 +1,21 @@
-export class CreateLectureDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Level } from "@prisma/client";
+import { IsInt, IsOptional, IsEnum, IsPositive } from "class-validator";
+
+export class CreateLectureDto {
+  @ApiProperty({ enum: Level })
+  @IsEnum(Level)
+  level: Level;
+
+  @IsOptional()
+  @IsInt()
+  @IsPositive()
+  lectureNumber?: number;
+
+  @IsInt()
+  bojGroupId: number;
+
+  @IsInt()
+  @IsPositive()
+  semesterId: number;
+}
--- apps/admin-backend/src/lecture/entities/lecture.entity.ts
+++ apps/admin-backend/src/lecture/entities/lecture.entity.ts
@@ -1,1 +1,11 @@
-export class Lecture {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Lecture, Level } from "@prisma/client";
+
+export class LectureEntity implements Lecture {
+  id: number;
+  @ApiProperty({ enum: Level })
+  level: Level;
+  lectureNumber: number;
+  bojGroupId: number;
+  semesterId: number;
+}
