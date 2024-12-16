fe06d75b91845f51f64629caf76a38b77ac03cd5
witch-factory <soakdma37@gmail.com>
2024-12-16 20:03:49 +0900

    feat: Lecture 기본 테스트 파일 삭제, prisma 모듈 추가

--- apps/admin-backend/src/lecture/lecture.module.ts
+++ apps/admin-backend/src/lecture/lecture.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
-import { LectureService } from './lecture.service';
-import { LectureController } from './lecture.controller';
+import { Module } from "@nestjs/common";
+import { LectureService } from "./lecture.service";
+import { LectureController } from "./lecture.controller";
+import { PrismaModule } from "src/prisma/prisma.module";
 
 @Module({
+  imports: [PrismaModule],
   controllers: [LectureController],
   providers: [LectureService],
 })
