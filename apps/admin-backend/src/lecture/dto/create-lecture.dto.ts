7296c4462cb68af2667c8c90907b5808e951828e
witch-factory <soakdma37@gmail.com>
2024-12-17 13:58:10 +0900

    minor: create lecture dto에 swagger 설명 추가

--- apps/admin-backend/src/lecture/dto/create-lecture.dto.ts
+++ apps/admin-backend/src/lecture/dto/create-lecture.dto.ts
@@ -3,18 +3,24 @@ import { Level } from "@prisma/client";
 import { IsInt, IsOptional, IsEnum, IsPositive } from "class-validator";
 
 export class CreateLectureDto {
-  @ApiProperty({ enum: Level })
+  @ApiProperty({
+    enum: Level,
+    description: "강의 난이도 (Novice, Intermediate, Advanced)",
+  })
   @IsEnum(Level)
   level: Level;
 
+  @ApiProperty({ description: "강의 회차수 (기본값: 10)", required: false })
   @IsOptional()
   @IsInt()
   @IsPositive()
   lectureNumber?: number;
 
+  @ApiProperty({ description: "BOJ 그룹 ID" })
   @IsInt()
   bojGroupId: number;
 
+  @ApiProperty({ description: "학기 ID (Semester의 id 값)" })
   @IsInt()
   @IsPositive()
   semesterId: number;
