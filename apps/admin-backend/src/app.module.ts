dc0bd36d35e3c6ca772e562a174f1bc279dba4e4
witch-factory <soakdma37@gmail.com>
2024-12-30 22:46:27 +0900

    feat: auth 모듈에 필요한 파일들 추가

--- apps/admin-backend/src/app.module.ts
+++ apps/admin-backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { SemesterModule } from "./semester/semester.module";
 import { LectureModule } from './lecture/lecture.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [SemesterModule, LectureModule],
+  imports: [SemesterModule, LectureModule, AuthModule],
 })
 export class AppModule {}
