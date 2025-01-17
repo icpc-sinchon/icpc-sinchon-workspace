ea88fba53cc9ea5a2db0ed8bb0f5f2269868436a
Junghwa <jungh150@naver.com>
2025-01-18 04:35:59 +0900

    feat: student service 생성

--- apps/admin-backend/src/student/student.module.ts
+++ apps/admin-backend/src/student/student.module.ts
@@ -1,12 +1,11 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "src/prisma/prisma.module";
 import { StudentRepository } from "./student.repository";
-//import { StudentService } from "./student.service";
-//import { StudentController } from "./student.controller";
+import { StudentService } from "./student.service";
 
 @Module({
   imports: [PrismaModule],
-  providers: [StudentRepository],
+  providers: [StudentRepository, StudentService],
   controllers: [],
 })
 export class StudentModule {}
