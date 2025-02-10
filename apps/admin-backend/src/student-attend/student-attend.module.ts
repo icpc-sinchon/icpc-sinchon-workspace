c81e03ec9080842cdd84150781d17009f49e2331
witch-factory <soakdma37@gmail.com>
2025-02-10 18:48:59 +0900

    feat: WeeklyAttendLogRepository를 student attend module에 추가

--- apps/admin-backend/src/student-attend/student-attend.module.ts
+++ apps/admin-backend/src/student-attend/student-attend.module.ts
@@ -4,10 +4,16 @@ import { StudentAttendController } from "./student-attend.controller";
 import { PrismaModule } from "@/prisma/prisma.module";
 import { StudentAttendRepository } from "./student-attend.repository";
 import { LectureRepository } from "@/lecture/lecture.repository";
+import { WeeklyAttendLogRepository } from "@/weekly-attend-log/weekly-attend-log.repository";
 
 @Module({
   imports: [PrismaModule],
   controllers: [StudentAttendController],
-  providers: [StudentAttendRepository, LectureRepository, StudentAttendService],
+  providers: [
+    StudentAttendRepository,
+    LectureRepository,
+    WeeklyAttendLogRepository,
+    StudentAttendService,
+  ],
 })
 export class StudentAttendModule {}
