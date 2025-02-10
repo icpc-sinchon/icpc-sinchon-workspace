a530c389beb7f79febf3da79a998ce0f0a2d4a82
witch-factory <soakdma37@gmail.com>
2025-02-10 18:16:31 +0900

    feat: 학생 출석 업데이트를 위한 dto

--- apps/admin-backend/src/student-attend/dto/update-student-attend.dto.ts
+++ apps/admin-backend/src/student-attend/dto/update-student-attend.dto.ts
@@ -1,4 +1,14 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateStudentAttendDto } from './create-student-attend.dto';
+import { PartialType } from "@nestjs/swagger";
+import { CreateStudentAttendDto } from "./create-student-attend.dto";
 
-export class UpdateStudentAttendDto extends PartialType(CreateStudentAttendDto) {}
+export class UpdateStudentAttendDto extends PartialType(
+  CreateStudentAttendDto
+) {}
+
+export class UpdateAttendanceDto {
+  studentId: number;
+  lectureId: number;
+  round: number;
+  lectureDone: boolean;
+  taskDone: boolean;
+}
